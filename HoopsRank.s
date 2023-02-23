7ea9ae9448b2fb9c79bd3330e3311494c59d8533
Philip Yi <philip.yi@duke.edu>
2023-02-22 22:08:53 -0500

    FINALLY FUCKING FINISHED

--- HoopsRank.s
+++ HoopsRank.s
@@ -15,23 +15,28 @@ main:
         li $a1, 63
         li $v0, 8
         syscall
-
         move $t0, $a0 #name stored in $t0
-        lb $t5, newline
-        la $t1, escape #puts DONE in $t1
-        _loop:
-            lb $t2, 0($t0)
-            lb $t3, 0($t1)
-            sub $t4, $t3, $t2
-            bne $t4, $0, _end_loop #branch doesn't match up so it is not DONE
+        la $a1, escape
+        jal strcmp
+        move $t1, $v0 #check if its eequal or not
+        beqz $t1, _break
+
+        # move $t0, $a0 #name stored in $t0
+        # lb $t5, newline
+        # la $t1, escape #puts DONE in $t1
+        # _loop:
+        #     lb $t2, 0($t0)
+        #     lb $t3, 0($t1)
+        #     sub $t4, $t3, $t2
+        #     bne $t4, $0, _end_loop #branch doesn't match up so it is not DONE
             
-            #if characters are equal
-            beq $t2, $t5, _break #if it reaches last character
-            #increment both addresses by one
-            addi $t0, $t0, 1
-            addi $t1, $t1, 1
-            j _loop
-        _end_loop:
+        #     #if characters are equal
+        #     beq $t2, $t5, _break #if it reaches last character
+        #     #increment both addresses by one
+        #     addi $t0, $t0, 1
+        #     addi $t1, $t1, 1
+        #     j _loop
+        # _end_loop:
         #asks for avg points
         la $a0, avg_pts
         li $v0, 4
@@ -91,8 +96,6 @@ main:
         move $a1, $s1
         jal insertNode #insert node into list
         move $s1, $v0 #makes new head
-        move $a0, $s1 #put head as an argument
-        jal printList
         j _while_loop
 
     _break:
